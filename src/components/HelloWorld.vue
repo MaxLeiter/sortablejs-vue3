81c4e0e5845cfdd06431f127514427dc567407e2
Pierre-Alban Lagadec <pierre-alban@hoalen.com>
2022-12-22 16:52:09 +0100

    ✨ - Implement default AutoScroll plugin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
 import Sortable from "./Sortable.vue";
 import { computed, ref } from "vue";
 import type { SortableOptions } from "sortablejs";
+import { AutoScrollOptions } from "sortablejs/plugins";
 
 const elements = computed(() => {
   return [
@@ -37,6 +38,74 @@ const elements = computed(() => {
       id: "3",
       text: "Three",
     },
+    {
+      id: "4",
+      text: "Four",
+    },
+    {
+      id: "5",
+      text: "Five",
+    },
+    {
+      id: "6",
+      text: "Six",
+    },
+    {
+      id: "7",
+      text: "Seven",
+    },
+    {
+      id: "8",
+      text: "Eight",
+    },
+    {
+      id: "9",
+      text: "Nine",
+    },
+    {
+      id: "10",
+      text: "Ten",
+    },
+    {
+      id: "11",
+      text: "Eleven",
+    },
+    {
+      id: "12",
+      text: "Twelve",
+    },
+    {
+      id: "13",
+      text: "Thirteen",
+    },
+    {
+      id: "14",
+      text: "Fourteen",
+    },
+    {
+      id: "15",
+      text: "Fifteen",
+    },
+    {
+      id: "16",
+      text: "Sixteen",
+    },
+    {
+      id: "17",
+      text: "Seventeen",
+    },
+    {
+      id: "18",
+      text: "Eighteen",
+    },
+    {
+      id: "19",
+      text: "Nineteen",
+    },
+    {
+      id: "20",
+      text: "Twenty",
+    },
   ];
 });
 
@@ -49,13 +118,20 @@ const logEvent = (evt: Event, evt2?: Event) => {
 };
 
 const animating = ref(true);
+const scrollSensitivity = ref(50);
+const scrollSpeed = ref(10);
 
-const options = computed<SortableOptions>(() => {
+const options = computed<SortableOptions | AutoScrollOptions>(() => {
   return {
     draggable: ".draggable",
     animation: animating.value ? 150 : 0,
     ghostClass: "ghost",
     dragClass: "drag",
+    scroll: true,
+    forceFallback: true,
+    scrollSensitivity: scrollSensitivity.value,
+    scrollSpeed: scrollSpeed.value,
+    bubbleScroll: true,
   };
 });
 
@@ -91,55 +167,91 @@ main {
 
 <template>
   <main>
-    <button @click="onPress">Toggle animations</button>
-    <Sortable
-      :list="elements"
-      item-key="id"
-      :options="options"
-      @change="logEvent"
-      @choose="logEvent"
-      @unchoose="logEvent"
-      @start="logEvent"
-      @end="logEvent"
-      @add="logEvent"
-      @update="logEvent"
-      @sort="logEvent"
-      @remove="logEvent"
-      @filter="logEvent"
-      @move="logEvent"
-      @clone="logEvent"
-    >
-      <template #item="{ element, index }">
-        <div class="draggable" :key="element.id">
-          {{ element.text }}
-          <Sortable
-            v-if="element.children"
-            :list="element.children"
-            :item-key="(item) => item.id"
-            :options="options"
-            @change="logEvent"
-            @choose="logEvent"
-            @unchoose="logEvent"
-            @start="logEvent"
-            @end="logEvent"
-            @add="logEvent"
-            @update="logEvent"
-            @sort="logEvent"
-            @remove="logEvent"
-            @filter="logEvent"
-            @move="logEvent"
-            @clone="logEvent"
-          >
-            <template #item="{ element, index }">
-              <div class="draggable" :key="element.id">
-                {{ element.text }}
-              </div>
-            </template>
-          </Sortable>
-        </div>
-      </template>
-    </Sortable>
+    <div class="settings">
+      <button @click="onPress">Toggle animations</button>
+      <div class="range">
+        <input
+          type="range"
+          min="0"
+          max="200"
+          v-model.number="scrollSensitivity"
+        />
+        <p>scrollSensitivity : {{ scrollSensitivity }}px</p>
+      </div>
+      <div class="range">
+        <input type="range" min="0" max="100" v-model.number="scrollSpeed" />
+        <p>scrollSpeed : {{ scrollSpeed }}px</p>
+      </div>
+    </div>
+    <div class="wrapper">
+      <Sortable
+        :list="elements"
+        item-key="id"
+        :options="options"
+        @change="logEvent"
+        @choose="logEvent"
+        @unchoose="logEvent"
+        @start="logEvent"
+        @end="logEvent"
+        @add="logEvent"
+        @update="logEvent"
+        @sort="logEvent"
+        @remove="logEvent"
+        @filter="logEvent"
+        @move="logEvent"
+        @clone="logEvent"
+      >
+        <template #item="{ element, index }">
+          <div class="draggable" :key="element.id">
+            {{ element.text }}
+            <Sortable
+              v-if="element.children"
+              :list="element.children"
+              :item-key="(item) => item.id"
+              :options="options"
+              @change="logEvent"
+              @choose="logEvent"
+              @unchoose="logEvent"
+              @start="logEvent"
+              @end="logEvent"
+              @add="logEvent"
+              @update="logEvent"
+              @sort="logEvent"
+              @remove="logEvent"
+              @filter="logEvent"
+              @move="logEvent"
+              @clone="logEvent"
+            >
+              <template #item="{ element, index }">
+                <div class="draggable" :key="element.id">
+                  {{ element.text }}
+                </div>
+              </template>
+            </Sortable>
+          </div>
+        </template>
+      </Sortable>
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  max-height: 400px;
+  overflow-y: auto;
+}
+
+.settings {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.settings .range {
+  display: flex;
+  flex-direction: column;
+}
+.settings .range p {
+  margin: 0;
+}
+</style>
